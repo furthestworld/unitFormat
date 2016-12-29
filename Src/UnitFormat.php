1dd9596609ca927ea5e6bc5649f8f582de05f60a
xuan <zhangpengxuan@yundun.com>
2016-12-29 10:35:14 +0800

    init proj

---
+++ Src/UnitFormat.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Desc: 数据单位格式化
+ * Created by PhpStorm.
+ * User: xuanskyer | <furthestworld@icloud.com>
+ * Date: 2016-12-29 10:33:51
+ */
+namespace FurthestWorld\UnitFormat\Src;
+
+
+class UnitFormat {
+
+
+}
---
+++ Tests/UnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 测试用例
+ * 这是一个很不规范的测试用例，请不要在意这些细节~~
+ */
+
+use FurthestWorld\UnitFormat\Src\UnitFormat;
+
+class UnitTest extends \PHPUnit_Framework_TestCase {
+
+
+}
+
